bf7ad47d37ce6497166156eedb75015e5dd93ae9
Иван <ivan1990kalinin@yandex.ru>
2024-05-22 20:22:46 +0700

    GPT. Нейросети спешат на помощь. [#505156]

--- src/main/java/ru/job4j/calculator/Fit.java
+++ src/main/java/ru/job4j/calculator/Fit.java
@@ -2,21 +2,36 @@ package ru.job4j.calculator;
 
 public class Fit {
 
+    // Constants for weight calculation
+    private static final double MAN_WEIGHT_COEFFICIENT = 1.15;
+    private static final double WOMAN_WEIGHT_COEFFICIENT = 1.15;
+    private static final int MAN_HEIGHT_OFFSET = 100;
+    private static final int WOMAN_HEIGHT_OFFSET = 110;
+
+    /**
+     * Calculates the ideal weight for a man based on height.
+     * @param height the height of the man in centimeters
+     * @return the ideal weight in kilograms
+     */
     public static double manWeight(short height) {
-        double result = (height - 100) * 1.15;
-        return result;
+        return (height - MAN_HEIGHT_OFFSET) * MAN_WEIGHT_COEFFICIENT;
     }
 
+    /**
+     * Calculates the ideal weight for a woman based on height.
+     * @param height the height of the woman in centimeters
+     * @return the ideal weight in kilograms
+     */
     public static double womanWeight(short height) {
-        double result = (height - 110) * 1.15;
-        return result;
+        return (height - WOMAN_HEIGHT_OFFSET) * WOMAN_WEIGHT_COEFFICIENT;
     }
 
     public static void main(String[] args) {
-        short height = 187;
+        short height = 175; // Height in centimeters
         double man = manWeight(height);
-        System.out.println("Man 187 is " + man);
+        System.out.println("Man " + height + " cm is " + man + " kg");
         double woman = womanWeight(height);
-        System.out.println("Woman 187 is " + woman);
+        System.out.println("Woman " + height + " cm is " + woman + " kg");
     }
 }
+
