1609fdc2b03f6279b3349a293483fc80395ff27b
Иван <ivan1990kalinin@yandex.ru>
2024-05-28 17:34:31 +0700

    5.1. Подсчет суммы чётных чисел в диапазоне [#192]

--- src/test/java/ru/job4j/loop/CounterTest.java
+++ src/test/java/ru/job4j/loop/CounterTest.java
@@ -50,4 +50,32 @@ class CounterTest {
         int output = Counter.sum(start, finish);
         assertThat(output).isEqualTo(expected);
     }
+
+    @Test
+    void whenSumEvenNumbersFromOneToTenThenThirty() {
+        int start = 1;
+        int finish = 10;
+        int result = Counter.sumByEven(start, finish);
+        int expected = 30;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenSumEvenNumbersFromFiveToTenThenTwentyFour() {
+        int start = 5;
+        int finish = 10;
+        int expected = 24;
+        int result = Counter.sumByEven(start, finish);
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenSumEvenNumbersFromTenToFiveThenNull() {
+        int start = 10;
+        int finish = 5;
+        int expected = 0;
+        int result = Counter.sumByEven(start, finish);
+        assertThat(result).isEqualTo(expected);
+
+    }
 }
