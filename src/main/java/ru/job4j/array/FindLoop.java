2319c18aeff1c56dabbc5c2e620b04730a981ba0
Иван <ivan1990kalinin@yandex.ru>
2024-05-30 19:53:56 +0700

    6.1. Классический поиск перебором. [#33489 #498451]

--- src/main/java/ru/job4j/array/FindLoop.java
+++ src/main/java/ru/job4j/array/FindLoop.java
@@ -6,6 +6,7 @@ public class FindLoop {
         for (int index = 0; index < data.length; index++) {
             if (data[index] == element) {
                 result = index;
+                break;
             }
         }
         return result;
